fbdc0dd5a3bf34c84ba461887b1b0a9a4b8e6397
ChavaMS <chava678ics@gmail.com>
2020-05-19 11:58:33 -0500

    fix inicio de sesion

--- views/iniciarSesion.view.php
+++ views/iniciarSesion.view.php
@@ -7,22 +7,20 @@
             <div class="card w-100">
                 <div class="card-body">
                     <h5 class="card-title text-center">Iniciar sesión</h5>
-                    <form>
-                        <div class="form-group">
-                            <label for="InputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="InputPassword1">Password</label>
-                            <input type="password" class="form-control" id="InputPassword1">
-                        </div>
-                        <div class="text-center">
-                            <button  onclick="crearSesion()" class="btn btn-primary">Iniciar sesión</button>
-                            <a class="d-block" href="#">¿Olvidaste tu contraseña?</a>
-                            <a class="d-block" href="registroOpcion.php">¿No tienes una cuenta? Registrate</a>
-                        </div>
-                    </form>
+                    <div class="form-group">
+                        <label for="InputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="InputPassword1">Password</label>
+                        <input type="password" class="form-control" id="InputPassword1">
+                    </div>
+                    <div class="text-center">
+                        <button onclick="crearSesion()" class="btn btn-primary">Iniciar sesión</button>
+                        <a class="d-block" href="#">¿Olvidaste tu contraseña?</a>
+                        <a class="d-block" href="registroOpcion.php">¿No tienes una cuenta? Registrate</a>
+                    </div>
                 </div>
             </div>
         </div>
